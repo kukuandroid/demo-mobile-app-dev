0c871802a0c63027d91aaf8021fb034f37486d28
KhairulFilhan <khairulfilhan94@gmail.com>
2025-08-27 23:31:12 +0800

    feat: Update app title to use AppStrings for consistency

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home/home_screen.dart';
+import 'config/strings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Movie App',
+      title: AppStrings.appTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
