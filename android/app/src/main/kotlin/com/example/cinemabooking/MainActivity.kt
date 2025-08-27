7b50ef0725536755cc3493ee162d28f3c82e2fbd
KhairulFilhan <khairulfilhan94@gmail.com>
2025-08-27 23:04:11 +0800

    cleanup

--- android/app/src/main/kotlin/com/example/cinemabooking/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cinemabooking
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/lib/src/core/config.dart
+++
@@ -1,5 +0,0 @@
-class AppConfig {
-  final String baseUrl;
-
-  AppConfig({String? baseUrl}) : baseUrl = baseUrl ?? 'http://10.0.2.2:4000';
-}
