96d4d4c8fa322f3229fb447bfee6897629ab86ac
KhairulFilhan <khairulfilhan94@gmail.com>
2025-08-28 00:38:22 +0800

    feat: Add additional fields to Movie model for detailed movie information

--- frontend/lib/models/movie.dart
+++ frontend/lib/models/movie.dart
@@ -3,11 +3,23 @@ class Movie {
   final String imageUrl;
   final String year;
   final String rating;
+  final String synopsis;
+  final List<String> cast;
+  final List<String> directors;
+  final List<String> writers;
+  final String runtime;
+  final List<String> genres;
 
   const Movie({
     required this.title,
     required this.imageUrl,
     required this.year,
     required this.rating,
+    required this.synopsis,
+    required this.cast,
+    required this.directors,
+    required this.writers,
+    required this.runtime,
+    required this.genres,
   });
 }
