af16b3fba58760848d6fecae4d00d989579b852e
KhairulFilhan <khairulfilhan94@gmail.com>
2025-08-28 12:20:20 +0800

    feat: Add booking summary route to AppRoutes for navigation

--- frontend/lib/routes/app_routes.dart
+++ frontend/lib/routes/app_routes.dart
@@ -1,3 +1,4 @@
+import 'package:frontend/screens/booking_summary/booking_summary_screen.dart';
 import 'package:frontend/screens/ticket_booking/ticket_booking_screen.dart';
 import 'package:get/get.dart';
 import '../screens/home/home_screen.dart';
@@ -7,6 +8,7 @@ class AppRoutes {
   static const String home = '/';
   static const String movieDetail = '/movie-detail';
   static const String ticketBooking = '/ticket-booking';
+  static const String bookingSummary = '/booking-summary';
 
   static List<GetPage> routes = [
     GetPage(name: home, page: () => const HomeScreen()),
@@ -18,6 +20,10 @@ class AppRoutes {
     GetPage(
       name: AppRoutes.ticketBooking,
       page: () => const TicketBookingScreen(),
+    ),
+    GetPage(
+      name: AppRoutes.bookingSummary,
+      page: () => const BookingSummaryScreen(),
     ),
   ];
 }
